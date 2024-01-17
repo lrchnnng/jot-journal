7eea8c58a6ffd32b333c6a5249a39e0a827a4387
Laura Channing <laurachanning@hotmail.co.uk>
2024-01-17 11:54:07 +0000

    changed date picker format

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
-    $('.datepicker').datepicker();
+    $('.datepicker').datepicker({
+      format: "dd mmmm yyyy,",
+      yearRange: 2,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
+  });
   });
