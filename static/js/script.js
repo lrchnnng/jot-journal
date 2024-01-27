cca31afeb211475c62da87f5606965d04c469b02
Laura Channing <laurachanning@hotmail.co.uk>
2024-01-27 20:59:24 +0000

    fixed styling for index page

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     $('.datepicker').datepicker({
-      format: "dd mmm yyyy,",
+      format: "dd mm yyyy,",
       yearRange: 100,
       showClearBtn: true,
       i18n: {
